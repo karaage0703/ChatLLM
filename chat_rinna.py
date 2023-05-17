248a6e01a75f6523c79714becae1d29d7d1ee8d7
karaage0703 <coldchicken@gmail.com>
2023-05-17 17:13:01 +0900

    Add chat function

--- chat_rinna.py
+++ chat_rinna.py
@@ -7,46 +7,32 @@ model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt-neox-3.6b-instr
 if torch.cuda.is_available():
     model = model.to("cuda")
 
-prompt = [
-    {
-        "speaker": "ユーザー",
-        "text": "日本のおすすめの観光地を教えてください。"
-    },
-    {
-        "speaker": "システム",
-        "text": "どの地域の観光地が知りたいですか？"
-    },
-    {
-        "speaker": "ユーザー",
-        "text": "渋谷の観光地を教えてください。"
-    }
-]
-prompt = [
-    f"{uttr['speaker']}: {uttr['text']}"
-    for uttr in prompt
-]
+messages = []
 
-prompt = "<NL>".join(prompt)
-prompt = (
-    prompt
-    + "<NL>"
-    + "システム: "
-)
-print(prompt)
+while True:
+    input_text = input('> ')
+    messages.append('ユーザー: ' + input_text)
+    # print(messages)
 
-token_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-
-with torch.no_grad():
-    output_ids = model.generate(
-        token_ids.to(model.device),
-        do_sample=True,
-        max_new_tokens=128,
-        temperature=0.7,
-        pad_token_id=tokenizer.pad_token_id,
-        bos_token_id=tokenizer.bos_token_id,
-        eos_token_id=tokenizer.eos_token_id
+    prompt = "<NL>".join(messages)
+    prompt = (
+        prompt
+        + "<NL>"
     )
+    token_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
 
-output = tokenizer.decode(output_ids.tolist()[0][token_ids.size(1):])
-output = output.replace("<NL>", "\n")
-print(output)
+    with torch.no_grad():
+        output_ids = model.generate(
+            token_ids.to(model.device),
+            do_sample=True,
+            max_new_tokens=128,
+            temperature=0.7,
+            pad_token_id=tokenizer.pad_token_id,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id
+        )
+
+    output = tokenizer.decode(output_ids.tolist()[0][token_ids.size(1):])
+    messages.append('システム: ' + output)
+    output = output.replace("<NL>", "\n")
+    print('リンナ: ' + output)
